fdb0a760ef8553de5a9c8f80c9cdb1e1d166b1f2
Nguyen Quyet Dinh <dinhnq@vccloud.vn>
2019-05-07 11:09:08 +0700

    deploy to heroku

--- app.py
+++ app.py
@@ -1,101 +1,64 @@
-import json
-import aiohttp
-from os import environ
-from aiohttp import web
+# Python libraries that we need to import for our bot
+import random
+from flask import Flask, request
+from pymessenger.bot import Bot
 
-# fanpage token
-PAGE_ACCESS_TOKEN = 'EAAEL3Fs9AZBsBAGKDiGoMyjkoxOIBzZBhEnJsidCYtMVpHiZBNCPKEiCQnhHuk4RAmsiVHMKEZB2ZCart9te7JJESxjfLzZArDZCNjs8WEl9xzTMT8gLFzcpW9Q1mjFJkCkZCZBL6KZBcNX50bKWsNHVXzhAWbKfNV0rRThohzdcf9nAZDZD'
-# verify token
+app = Flask(__name__)
+ACCESS_TOKEN = 'EAAEL3Fs9AZBsBAGKDiGoMyjkoxOIBzZBhEnJsidCYtMVpHiZBNCPKEiCQnhHuk4RAmsiVHMKEZB2ZCart9te7JJESxjfLzZArDZCNjs8WEl9xzTMT8gLFzcpW9Q1mjFJkCkZCZBL6KZBcNX50bKWsNHVXzhAWbKfNV0rRThohzdcf9nAZDZD'
 VERIFY_TOKEN = 'dinhpu@Vcc123**'
+bot = Bot(ACCESS_TOKEN)
 
 
-class BotControl(web.View):
-
-    async def get(self):
-        query = self.request.rel_url.query
-        if (query.get('hub.mode') == "subscribe" and query.get("hub.challenge")):
-            if not query.get("hub.verify_token") == VERIFY_TOKEN:
-                return web.Response(text='Verification token mismatch', status=403)
-            return web.Response(text=query.get("hub.challenge"))
-        return web.Response(text='Forbidden', status=403)
-
-    async def post(self):
-        data = await self.request.json()
-        if data.get("object") == "page":
-            await self.send_greeting("Chào bạn. Mình là bot demo của học python.")
-
-            for entry in data.get("entry"):
-                for messaging_event in entry.get("messaging"):
-                    if messaging_event.get("message"):
-                        sender_id = messaging_event["sender"]["id"]
-                        message_text = messaging_event["message"]["text"]
-
-                        if any(["chào" in message_text.lower(), "hi " in message_text.lower(),
-                                "hello" in message_text.lower(), "có ai" in message_text.lower(),
-                                "có ở đó" in message_text.lower(), "hi" == message_text.lower()]):
-                            await self.send_message(sender_id, "chào đằng ấy :)")
-                        elif any(["bạn tên" in message_text.lower(), "mày tên" in message_text.lower(),
-                                  "your name" in message_text.lower(), "cậu tên" in message_text.lower()]):
-                            await self.send_message(sender_id, "mình tên là bot demo aiohttp nha")
-                        elif any(["tác giả" in message_text.lower(), "người viết" in message_text.lower(),
-                                  "ai viết" in message_text.lower(), "ba mày" in message_text.lower(),
-                                  "cha mày" in message_text.lower()
-                                     , "bố mày" in message_text.lower(), "tía mày" in message_text.lower()]):
-                            await self.send_message(sender_id,
-                                                    "ahihi bạn vào đây để xem ai là người tạo ra mình nha :3 https://www.hocpython.com")
-                        else:
-                            await self.send_message(sender_id,
-                                                    "Bạn dễ thương gì ấy ơi, ghé https://www.hocpython.com để ủng hộ ba mình nha :3 ")
-                            await self.send_message(sender_id,
-                                                    "mình nghe ba mình nói nếu mình được 100 like sẽ chia sẻ với các bạn thêm tính năng mới của mình đó :3")
-
-        return web.Response(text='ok', status=200)
-
-    async def send_greeting(self, message_text):
-        params = {
-            "access_token": PAGE_ACCESS_TOKEN
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "setting_type": "greeting",
-            "greeting": {
-                "text": message_text
-            }
-        })
-        async with aiohttp.ClientSession() as session:
-            await session.post("https://graph.facebook.com/v3.0/me/thread_settings", params=params, headers=headers,
-                               data=data)
-
-    async def send_message(self, sender_id, message_text):
-
-        params = {
-            "access_token": PAGE_ACCESS_TOKEN
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "recipient": {
-                "id": sender_id
-            },
-            "message": {
-                "text": message_text
-            }
-        })
-
-        async with aiohttp.ClientSession() as session:
-            await session.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
+# We will receive messages that Facebook sends our bot at this endpoint
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    # if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    # Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    # if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
 
 
-routes = [
-    web.get('/', BotControl, name='verify'),
-    web.post('/', BotControl, name='webhook'),
-]
+def verify_fb_token(token_sent):
+    # take token sent by facebook and verify it matches the verify token you sent
+    # if they match, allow the request, else return an error
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
 
-app = web.Application()
-app.add_routes(routes)
 
-# if __name__ == '__main__':
-#     web.run_app(app, host='0.0.0.0', port=environ.get("PORT", 9090))
+# chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!",
+                        "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+
+# uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    # sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+
+if __name__ == "__main__":
+    app.run()
